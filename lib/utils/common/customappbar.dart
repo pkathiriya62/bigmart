00244432dccf19d2b94de4f79e642e5632a63622
Prince kathiriya <princekathiriya62@gmail.com>
2024-01-27 13:02:49 +0530

    cart

--- lib/utils/common/customappbar.dart
+++ lib/utils/common/customappbar.dart
@@ -1,13 +1,22 @@
 import 'package:bigmart/utils/common/appcolor.dart';
+// import 'package:bigmart/utils/common/apptext.dart';
 import 'package:bigmart/utils/common/globaltext.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomContainer extends StatelessWidget {
   final String text;
-  final  subtext;
+  final subtext;
+  final double? fontsize;
+  final Color? color;
 
-  const CustomContainer({super.key, required this.text, this.subtext});
+  const CustomContainer({
+    super.key,
+    required this.text,
+    this.subtext,
+    this.fontsize, this.color,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -25,54 +34,84 @@ class CustomContainer extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
           children: [
-            SizedBox(
-              height: height * 0.005,
-            ),
             Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
+                Expanded(
+                  child: Container(
+                    height: height * 0.105,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Globaltext(
+                                  text: text,
+                                  fontfamily: GoogleFonts.inter().toString(),
+                                  fontsize: fontsize ?? 20,
+                                  color: Colors.white,
+                                ),
+                                Globaltext(
+                                  text: subtext ?? "",
+                                  color: color ?? Colors.white,
+                                  fontsize: 10,
+                                  fontfamily: GoogleFonts.inter().toString(),
+
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                SizedBox(
-                  width: width * 0.012,
-                ),
-                Globaltext(
-                  text: text,
-                  fontfamily: GoogleFonts.inter().toString(),
-                  fontsize: 20,
-                  color: Colors.white,
-                ),
-                Spacer(),
                 Container(
-                  height: height * 0.100,
-                  width: width * 0.100,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColor.homecontainercolor,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: height * 0.100,
+                        width: width * 0.100,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColor.homecontainercolor,
+                        ),
+                        child: const Icon(
+                          Icons.shopping_cart_outlined,
+                          // size: 25,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        width: width * 0.012,
+                      ),
+                      Container(
+                        height: height * 0.10,
+                        width: width * 0.100,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColor.homecontainercolor,
+                        ),
+                        child: const Icon(
+                          Icons.menu,
+                          // size: 30.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-                  child: const Icon(
-                    Icons.shopping_cart_outlined,
-                    // size: 25,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(
-                  width: width * 0.012,
-                ),
-                Container(
-                  height: height * 0.10,
-                  width: width * 0.100,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColor.homecontainercolor,
-                  ),
-                  child: const Icon(
-                    Icons.menu,
-                    // size: 30.0,
-                    color: Colors.white,
-                  ),
-                ),
+                )
               ],
             ),
           ],
--- lib/view/screenshome/cartscreen.dart
+++ lib/view/screenshome/cartscreen.dart
@@ -1,4 +1,11 @@
+import 'package:bigmart/utils/common/appcolor.dart';
+import 'package:bigmart/utils/common/apptext.dart';
+import 'package:bigmart/utils/common/customappbar.dart';
+
+import 'package:bigmart/utils/common/globaltext.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -7,11 +14,209 @@ class CartScreen extends StatefulWidget {
   State<CartScreen> createState() => _CartScreenState();
 }
 
+class CartName {
+  String image;
+  String name;
+  String subname;
+  double price;
+
+  CartName(
+      {required this.name,
+      required this.subname,
+      required this.image,
+      required this.price});
+}
+
+List<CartName> cartdata = [
+  CartName(
+    name: "Chocolate",
+    subname: 'Bittersweet Chocolate',
+    image: 'assets/image/cart1.png',
+    price: 120,
+  ),
+  CartName(
+    name: "Egg",
+    subname: 'Egg box',
+    image: 'assets/image/cart2.png',
+    price: 80,
+  ),
+  CartName(
+    name: "Butter",
+    subname: 'Vegetable oil butter',
+    image: 'assets/image/cart3.png',
+    price: 120,
+  ),
+  CartName(
+    name: "Drinks",
+    subname: 'Kanguru energy drink',
+    image: 'assets/image/cart4.png',
+    price: 80,
+  ),
+  CartName(
+    name: "Kraft",
+    subname: 'Thousand island drink drink drink',
+    image: 'assets/image/cart5.png',
+    price: 80,
+  ),
+];
+
 class _CartScreenState extends State<CartScreen> {
+  int selectedindex = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('cart')),
+      body: Column(
+        children: [
+          CustomContainer(
+            text: Apptext.carttext,
+            subtext: Apptext.cartsubtext,
+            fontsize: 14,
+            color: AppColor.cartsubnamecolor,
+          ),
+          ListView.builder(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            itemCount: cartdata.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 22.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: SizedBox(
+                            height: 100.h,
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: 90.w,
+                                  child: Image.asset(
+                                    cartdata[index].image,
+                                    scale: 3,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 9.w,
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Globaltext(
+                                        textStyle: const TextStyle(
+                                            overflow: TextOverflow.ellipsis),
+                                        text: cartdata[index].name,
+                                        fontfamily:
+                                            GoogleFonts.inter().toString(),
+                                        fontweight: FontWeight.w400,
+                                        color: AppColor.cartcardtextcolor,
+                                      ),
+                                      SizedBox(
+                                        height: 8.h,
+                                      ),
+                                      Globaltext(
+                                        text: cartdata[index].subname,
+                                        textStyle: TextStyle(
+                                            overflow: TextOverflow.ellipsis,
+                                            fontWeight: FontWeight.w500,
+                                            color: AppColor.blackcolor),
+                                      ),
+                                      SizedBox(
+                                        height: 3.h,
+                                      ),
+                                      Globaltext(
+                                        text:
+                                            "₹ ${cartdata[index].price.toStringAsFixed(0)}",
+                                        fontfamily:
+                                            GoogleFonts.inter().toString(),
+                                        fontweight: FontWeight.w500,
+                                        color: AppColor.blackcolor,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+
+                              height: 24.h,
+                              width: 24.w,
+                              decoration: BoxDecoration(
+                                  color: AppColor.cartcontainercolor,
+                                  shape: BoxShape.circle),
+                              child: Icon(
+                                Icons.remove,
+                                size: 15,
+                                color: AppColor.primarycolor,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 9.w,
+                            ),
+                            Globaltext(text: selectedindex.toString()),
+                            SizedBox(
+                              width: 9.w,
+                            ),
+                            Container(
+                              height: 24.h,
+                              width: 24.w,
+                              decoration: BoxDecoration(
+                                  color: AppColor.cartcontainercolor,
+                                  shape: BoxShape.circle),
+                              child: Icon(
+                                Icons.add,
+                                size: 15,
+                                color: AppColor.primarycolor,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  const Divider()
+                ],
+              );
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 22.0),
+            child: SizedBox(
+              height: 48.h,
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStatePropertyAll(AppColor.primarycolor),
+                  shape: const MaterialStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  "3 items  ₹ 750      Proceed To Pay >>",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
