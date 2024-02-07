2c281aabc11b5cdbb10cb87320cf3dcfdb96edef
Prince kathiriya <princekathiriya62@gmail.com>
2024-02-07 18:05:18 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:bigmart/firebase_options.dart';
 import 'package:bigmart/view/Auth/signin.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
